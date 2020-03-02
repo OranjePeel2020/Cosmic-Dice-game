89f4c06e2a768d451b51be685ce9c1d46dad4270
OranjePeelStudios <60318150+OranjePeel2020@users.noreply.github.com>
2020-03-02 08:57:44 +0200

    Add files via upload

---
+++ eng-dice.js
@@ -0,0 +1,23 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "images/engdice" + randomNumber1 + ".png";
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomDiceImage);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "images/engdice" + randomNumber2 + ".png";
+
+var image1 = document.querySelectorAll("img")[1];
+
+image1.setAttribute("src", randomDiceImage);
+
+if (randomNumber1 > randomNumber2) {
+   document.querySelectorAll("h1")[0].innerHTML = ("Player 1 WINS!🚩");
+} else if (randomNumber2 > randomNumber1) {
+   document.querySelectorAll("h1")[0].innerHTML = ("Player 2 WINS!🚩");
+} else if (randomNumber2 === randomNumber1){
+  document.querySelectorAll("h1")[0].innerHTML = ("Draw 🎌");
+};
