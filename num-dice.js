202b9394b4d03feff54ff402acd7567b4fcabc33
OranjePeelStudios <60318150+OranjePeel2020@users.noreply.github.com>
2020-03-02 09:10:58 +0200

    Update num-dice.js

--- num-dice.js
+++ num-dice.js
@@ -1,6 +1,6 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage = "images/nodice" + randomNumber1 + ".png";
+var randomDiceImage = "nodice" + randomNumber1 + ".png";
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -8,7 +8,7 @@ image1.setAttribute("src", randomDiceImage);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage = "images/nodice" + randomNumber2 + ".png";
+var randomDiceImage = "nodice" + randomNumber2 + ".png";
 
 var image1 = document.querySelectorAll("img")[1];
 
