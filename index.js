7996914e5e58cfa2ed2c69d22b458c36eb2265d6
OranjePeelStudios <60318150+OranjePeel2020@users.noreply.github.com>
2020-03-02 09:09:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+var randomDiceImage = "dice" + randomNumber1 + ".png";
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -10,7 +10,7 @@ image1.setAttribute("src", randomDiceImage);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage = "images/dice" + randomNumber2 + ".png";
+var randomDiceImage = "dice" + randomNumber2 + ".png";
 
 var image1 = document.querySelectorAll("img")[1];
 
