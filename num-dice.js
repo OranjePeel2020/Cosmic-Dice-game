181ea20059924b43b9b74b8c8eff3e2bc7901742
OranjePeelStudios <60318150+OranjePeel2020@users.noreply.github.com>
2020-03-04 14:49:18 +0200

    Update num-dice.js

--- num-dice.js
+++ num-dice.js
@@ -1,3 +1,13 @@
+function namePlayers() {
+	var player1 = document.getElementById("player1").value;
+	var player2 = document.getElementById("player2").value;
+
+	var player1 = player1.toUpperCase();
+	var player2 = player2.toUpperCase();
+
+	document.querySelectorAll("p")[0].innerHTML = player1;
+	document.querySelectorAll("p")[1].innerHTML = player2;
+
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage = "nodice" + randomNumber1 + ".png";
@@ -21,3 +31,32 @@ if (randomNumber1 > randomNumber2) {
 } else if (randomNumber2 === randomNumber1){
   document.querySelectorAll("h1")[0].innerHTML = ("Draw 🎌");
 };
+   
+   }
+
+var form = document.getElementById("myForm");
+function handleForm(event) { event.preventDefault(); }
+form.addEventListener('submit', handleForm);
+
+ window.onload = function() {
+
+// If sessionStorage is storing default values (ex. name), exit the function and do not restore data
+if (sessionStorage.getItem('name') == "player1") {
+	return;
+}
+
+// If values are not blank, restore them to the fields
+var name = sessionStorage.getItem('name');
+if (name !== null) $('#player1').val(name);
+
+var email = sessionStorage.getItem('email');
+if (email !== null) $('#player2').val(email);
+
+}
+
+// Before refreshing the page, save the form data to sessionStorage
+window.onbeforeunload = function() {
+sessionStorage.setItem("name", $('#player1').val());
+sessionStorage.setItem("email", $('#player2').val());
+}
+
